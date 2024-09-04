ff14aedb51f7217d4d4eb0aef3667ec8a9c600a8
Ian <Kaialexis0613@gmail.com>
2024-09-04 14:37:11 +0530

    client_test update

--- client3.py
+++ client3.py
@@ -31,18 +31,18 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price+ask_price) / 2
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return price_a/price_b
+    if price_b == 0:
+        return None  # Return None to avoid division by zero
+    return price_a / price_b
 
 
 # Main
@@ -51,11 +51,16 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
-        prices={}
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            prices[stock]=price
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
+        # Calculate and print the ratio if both prices are available
+        if "ABC" in prices and "DEF" in prices:
+            ratio = getRatio(prices["ABC"], prices["DEF"])
+            if ratio is not None:
+                print("Ratio %s" % ratio)
+            else:
+                print("Ratio could not be calculated due to division by zero.")
